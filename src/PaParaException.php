df934253126c652b2d215958b8b80dab1399fd1a
Üsame Fethullah AVCI <u@gaaraj.com>
2017-03-27 23:15:27 +0300

    papara exception added

---
+++ src/PaParaException.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace UsameAvci\PaPara;
+
+use Exception;
+
+/**
+* PaPara Exception class
+*/
+class PaParaException extends Exception
+{
+    public function __construct($message, $code = 0, Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function __toString()
+    {
+        return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+    }
+}
